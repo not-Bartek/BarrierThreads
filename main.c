92adc86d342cfd194a7bdda721753b6b3f7808b6
Bartosz Dźwigała <b.dzwigala05@gmail.com>
2026-01-19 20:02:16 +0100

    create threads

--- main.c
+++ main.c
@@ -19,6 +19,26 @@ struct arguments
     pthread_barrier_t *barrier;
 };
 
+void* thread_func(void *arg) {
+    
+}
+
+void create_threads(pthread_t *thread, struct arguments *targ, pthread_barrier_t *barrier, int *scores, int* rolls)
+{
+    srand(time(NULL));
+    int i;
+    for (i = 0; i < PLAYER_COUNT; i++)
+    {
+        targ[i].id = i;
+        targ[i].seed = rand();
+        targ[i].scores = scores;
+        targ[i].rolls = rolls;
+        targ[i].barrier = barrier;
+        if (pthread_create(&thread[i], NULL, thread_func, (void *)&targ[i]) != 0)
+            ERR("pthread_create");
+    }
+}
+
 int main() {
     return 0;
 }
